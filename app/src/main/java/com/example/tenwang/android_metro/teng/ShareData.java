d7e960e3fec2a69ccde229773dcc3a5c1feefcaa
Teng Wang <markzog21@gmail.com>
2016-05-17 12:42:30 +0800

    Update ShareData.java

--- app/src/main/java/com/example/tenwang/android_metro/teng/ShareData.java
+++ app/src/main/java/com/example/tenwang/android_metro/teng/ShareData.java
@@ -3,7 +3,6 @@ package com.example.tenwang.android_metro.teng;
 import android.widget.GridLayout;
 
 public class ShareData {
-	//the singleton
 	public static ShareData shareData;
 	public static ShareData data() {
 		if (shareData == null)
